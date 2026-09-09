5b34d095cc48061b7009c531549e08b154f58604
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 12:22:09 -0300

    fix(test): load servlet security context in integration test

--- src/test/java/cl/helvoca/booking/BookingRepositoryIntegrationTest.java
+++ src/test/java/cl/helvoca/booking/BookingRepositoryIntegrationTest.java
@@ -20,7 +20,7 @@ import java.time.Instant;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class BookingRepositoryIntegrationTest {
 
     @Container
