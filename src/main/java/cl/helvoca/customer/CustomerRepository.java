708007e2db3111a8ec943bb034754ae00467d33e
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 11:02:01 -0300

    feat: add customer repository

---
+++ src/main/java/cl/helvoca/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package cl.helvoca.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findAllByBusinessIdOrderByCreatedAtDesc(UUID businessId);
+    Optional<Customer> findByIdAndBusinessId(UUID id, UUID businessId);
+    Optional<Customer> findFirstByBusinessIdAndPhone(UUID businessId, String phone);
+}
