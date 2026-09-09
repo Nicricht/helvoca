2eda192a3646c81d30f1efe2ee44169e2222145e
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 10:47:01 -0300

    feat: add user repository

---
+++ src/main/java/cl/helvoca/user/AppUserRepository.java
@@ -0,0 +1,12 @@
+package cl.helvoca.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+    Optional<AppUser> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+    List<AppUser> findAllByBusinessIdOrderByName(UUID businessId);
+}
