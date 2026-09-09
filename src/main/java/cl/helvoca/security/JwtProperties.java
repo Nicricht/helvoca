d13b02ed0a80049c07cc98cc02aca522dc296544
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 10:44:33 -0300

    feat: add JWT properties

---
+++ src/main/java/cl/helvoca/security/JwtProperties.java
@@ -0,0 +1,10 @@
+package cl.helvoca.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.jwt")
+public record JwtProperties(
+        String issuer,
+        String secret,
+        long accessTokenMinutes) {
+}
