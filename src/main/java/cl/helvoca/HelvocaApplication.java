b22a6dc17d7cd6451b5f74aba919ecd64684b6e2
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 17:34:22 -0300

    feat(call): enable asynchronous summary generation

--- src/main/java/cl/helvoca/HelvocaApplication.java
+++ src/main/java/cl/helvoca/HelvocaApplication.java
@@ -2,7 +2,9 @@ package cl.helvoca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class HelvocaApplication {
     public static void main(String[] args) {
