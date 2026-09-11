6ec4d4d0a6e3fdf1ae8bd0133124a29641098720
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-11 10:04:54 -0300

    fix: make Twilio call control constructor injectable

--- src/main/java/cl/helvoca/telephony/twilio/TwilioCallControl.java
+++ src/main/java/cl/helvoca/telephony/twilio/TwilioCallControl.java
@@ -2,6 +2,7 @@ package cl.helvoca.telephony.twilio;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -31,6 +32,7 @@ public class TwilioCallControl {
     private final HttpClient http;
     private final String apiBase;
 
+    @Autowired
     public TwilioCallControl(TwilioProperties properties) {
         this(properties,
                 HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(8)).build(),
